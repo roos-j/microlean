efc175c45aecb06cc930801e06dae609aa690afa
Joris Roos <roos.jor@gmail.com>
2026-06-05 12:43:00 +0200

    finish whnf_core

--- src/type_checker.zig
+++ src/type_checker.zig
@@ -37,20 +37,41 @@ pub const TypeChecker = struct {
         allocator.destroy(self);
     }
 
-    /// Return weak head normal form, 'core' variant - here only beta reduction
+    /// Return weak head normal form, 'core' variant - here only beta reduction.
+    /// This is a purely syntactic operation, no type checking.
     pub fn whnf_core(self: *Self, e: Expr) Expr {
-        // const em = self.em;
-        if (!e.isLambda()) return e;
+        if (!e.isApp()) return e;
         if (self.whnf_cache[0].get(e)) |whnf_e| return whnf_e;
 
-        const whnf_e: Expr = undefined;
+        const em = self.em;
+        const es = self.es;
+        var whnf_e: Expr = undefined;
+
         self.get_args_buf.clear();
-        //var f = em.getAppArgsRev(e, &self.get_args_buf);
-        // if (f.isLambda()) { // Beta reduction
-        //     // Todo
-        // } else { // Cannot beta reduce, so nothing to do
-        //     whnf_e = e;
-        // }
+        var f = em.getAppArgsRev(e, &self.get_args_buf);
+        if (f.isLambda()) { // Beta reduction
+            var n_lambdas: u32 = 1; // Count nested lambdas
+            const n_apps = self.get_args_buf.len();
+            var body = em.getLambda(f).body;
+            while (body.isLambda() and n_lambdas < n_apps) {
+                n_lambdas += 1;
+                body = em.getLambda(body).body;
+            }
+            std.debug.assert(n_lambdas <= n_apps);
+            // E.g. app( app( app (app (fun a => fun b => body[#0,#1]) arg0) arg1) arg2) arg3
+            // Then args_buf = [arg3, arg2, arg1, arg0]
+            // Should become: app (app body[arg1,arg0] arg2) arg3
+            const cutoff_idx = n_apps - n_lambdas;
+            const subst_args = self.get_args_buf.items()[cutoff_idx..];
+            const body_subst = es.substLooseBvars(body, 0, subst_args);
+            // Recover the unconsumed app's
+            const remaining_args = self.get_args_buf.items()[0..cutoff_idx];
+            whnf_e = es.mkAppArgsRev(body_subst, remaining_args);
+            // Head could be a reducible app again, so recurse
+            whnf_e = self.whnf_core(whnf_e);
+        } else { // Cannot beta reduce, so nothing to do
+            whnf_e = e;
+        }
 
         self.whnf_cache[0].put(e, whnf_e);
         return whnf_e;
