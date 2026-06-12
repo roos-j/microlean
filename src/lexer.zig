24a7399f5680a507f6b0e0d5d899e514c7799109
Joris Roos <roos.jor@gmail.com>
2026-06-12 10:24:27 +0200

    finish lexer

--- src/lexer.zig
+++ src/lexer.zig
@@ -5,71 +5,70 @@ pub fn utf8error() noreturn {
     @panic("malformed utf-8 detected");
 }
 
-// command1
-// command2
-
-// Token categories:
-// keyword -- built-in keyword such as import, def, etc.
-// ident -- identifier
-// punct -- punctuation symbol, not necessarily on: parentheses, colon, :=
-// 
-// comment
-
-// Possible commands:
-// `import` <filename>
-// `def` <string> (<string> : term) : term := term
-
-// TODO: add Nat literals?
 pub const TokenKind = enum {
     eof,
     invalid,
     ident, // identifier
-    lit, // numeric literal
+    numlit, // numeric literal
 
     // Atoms:
 
-    // keywords
     def,
     axiom,
+    check,
     universe,
     sort,
     prop,
     lambda,
-    pi,
-    // punctuation
-    to,
-    mapsto,
-    coloneq,
-    l_paren,
-    r_paren,
-    colon,
+    import,
+
+    lparen, // (
+    rparen, // ) 
+    colon, // :
+
+    coloneq, // :=
+    to, // → or -> 
+    mapsto, // ↦ or =>
+    forall, // forall or ∀ or Π
+    
 };
 
+// TODO: add builtin arithmetic operators +, -, *, ^, \cdot, =, <, <=, >, >=
+// add more commands, add inductive type keywords
 const builtinAtoms = std.StaticStringMap(TokenKind).initComptime(.{
     .{ "def", .def },
     .{ "axiom", .axiom },
-    .{ "->", .to },
-    .{ ":", .colon },
-    .{ ":=", .coloneq }
+    .{ "#check", .check },
+    .{ "universe", .universe },
+    .{ "import", .import },
+    .{ "Sort", .sort },
+    .{ "Prop", .prop },
+    .{ "fun", .lambda },
+    .{ "λ", .lambda },
+    .{ "forall", .forall },
+    .{ "∀", .forall },
+    .{ "Π", .forall },
+    .{ "↦", .mapsto },
+    .{ "=>", .mapsto },
+    .{ ":=", .coloneq },
+    .{ "→", .to },
+    .{ "->", .to }
 });
 
-fn isIdentStart(c: u21) bool {
-    return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or c >= 0x80;
+fn isIdentAscii(c: u8) bool {
+    // Non-ascii characters are caught by special handler
+    return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or c == '_';
 }
 
-fn isCmdStart(c: u21) bool {
-    return c == '#';
-}
-
-fn isNumLit(c: u21) bool {
+fn isNumLit(c: u8) bool {
     return c >= '0' and c <= '9';
 }
 
-fn isIdentCont(c: u21) bool {
-    return isIdentStart(c) or isNumLit(c) or c == '\'';
+fn isIdentCont(c: u8) bool {
+    return isIdentAscii(c) or isNumLit(c) or c == '\'' or c >= 0x80;
 }
 
-fn isWhitespace(c: u21) bool {
+fn isWhitespace(c: u8) bool {
     return c == ' ' or c == '\t' or c == '\n' or c == '\r';
 }
 
@@ -82,55 +81,60 @@ pub const Token = struct {
     len: u32, // total lexeme length
     offset: u32, // start of the actual Token (= length of leading trivia)
 
+    /// A token is an atom if it is not an identifier, eof, literal or invalid.
     pub fn isAtom(t: Token) bool {
         return !t.isIdent() and !t.isInvalid() and !t.isEOF() and !t.isLiteral();
     }
-    pub fn isLiteral(t: Token) bool { return t.kind == .lit; }
+    pub fn isLiteral(t: Token) bool { return t.kind == .numlit; }
     pub fn isIdent(t: Token) bool { return t.kind == .ident; }
     pub fn isInvalid(t: Token) bool { return t.kind == .invalid; }
     pub fn isEOF(t: Token) bool { return t.kind == .eof; }
 
-    // Return a token's main content. Assume that token is well-formed.
+    /// Return a token's main content. Assume that token is well-formed.
     pub fn token(t: Token, l: *Lexer) []const u8 {
         return l.src[t.source_offset+t.offset..t.source_offset+t.len];
     }
 
-    // Return a token's leading trivia.
+    /// Return a token's leading trivia.
     pub fn leading(t: Token, l: *Lexer) []const u8 {
         return l.src[t.source_offset..t.source_offset+t.offset];
     }
 
-    // Return complete lexeme of a token.
+    /// Return complete lexeme of a token, including leading whitespace.
     pub fn lexeme(t: Token, l: *Lexer) []const u8 {
         return l.src[t.source_offset..t.source_offset+t.len];
     }
 
-    // Return true if token has leading whitespace (whitespace is anything that is not part of main token content).
+    /// Return true if token has leading whitespace (whitespace is anything that is not part of main token content).
     pub fn leadingWs(t: Token) bool {
         return t.offset > 0;
     }
 
-    // Return true if token has a leading newline.
+    /// Return true if token has at least one newline in it's leading whitespace.
     pub fn leadingNewline(t: Token) bool {
         return t.has_newline;
     }
 };
 
-pub const LexerState = enum {
-    init, 
-    trivia, // first we read trivia
-    skip_whitespace,
-    line_cmt,
-    multiline_cmt,
-};
+// pub const LexerState = enum {
+//     init, 
+//     trivia, // first we read trivia
+//     skip_whitespace,
+//     line_cmt,
+//     multiline_cmt,
+// };
 
+/// Simple lexer for MicroLean's syntax. The goal is to support a subset of Lean's syntax.
+/// It isn't a proper subset because of various edge cases for now, but for most practical purposes should be fine.
+/// Note actual Lean is lexer-less to allow
+/// for flexible syntax extensions within Lean itself.
 pub const Lexer = struct {
     const Self = @This();
 
     src: []const u8, // Source code
     cur: usize = 0, // Points to next character in source to be read
     
-    state: LexerState = .init, // The lexer is a finite state machine
+    // state: LexerState = .init, // The lexer is a finite state machine
     cur_t: Token, // Used to keep state while reading token
 
     pub fn init(src: []const u8) Lexer {
@@ -143,29 +147,122 @@ pub const Lexer = struct {
 
     // Read next token.
     pub fn next(self: *Self) Token {
-        if (self.isAtEnd()) return mkEmptyEOF();
+        self.initToken();
+        // if (self.isAtEnd()) return self.mkToken(.eof);
 
-        // Cases
-        // Identifier syntax: [a-zA-Z]([\.]?[a-zA-Z0-9]+)*
-        // Keyword syntax: [#]?identifier
-        // Literal: [0-9]+
-        // Punctuation: ( ) := :
-        // Newline: \n
-        // 
+        self.whitespace(); // skip trivia
+        if (self.isAtEnd()) return self.mkToken(.eof);
+
+        const c = self.peek(0);
+
+        switch (c) {
+            '(' => { self.skip(); return self.mkToken(.lparen); },
+            ')' => { self.skip(); return self.mkToken(.rparen); },
+            ':' => return self.handleColon(),
+            '-' => return self.handleDash(),
+            '=' => return self.handleEq(),
+            '#' => return self.handleCmd(),
+            else => {}
+        }
+
+        if (c >= 0x80) {
+            // Check if this is a non-ascii single character atom like `→`
+            const full_c = self.peekFull();
+            if (builtinAtoms.get(full_c)) |kind| {
+                self.cur = @min(self.src.len, self.cur + full_c.len);
+                return self.mkToken(kind);
+            } else {
+                // If not, for simplicity run identifier or keyword handler.
+                // TODO: More careful with validation
+                return self.identOrKeyword();
+            }
+        }
+
+        if (isIdentAscii(c)) return self.identOrKeyword()
+        else if (isNumLit(c)) return self.numLit()
+        else {
+            self.skip();
+            return self.mkToken(.invalid);
+        }
+    }
+
+    // Handle command starting on '#'
+    fn handleCmd(self: *Self) Token {
+        self.skip();
+        while (isIdentAscii(self.peek(0))) self.skip();
+        const token = self.src[self.cur_t.source_offset+self.cur_t.offset..self.cur];
+        if (builtinAtoms.get(token)) |kind| {
+            return self.mkToken(kind);
+        } else {
+            // @panic("expected valid command after '#'");
+            return self.mkToken(.invalid);
+        }
+    }
+
+    fn initToken(self: *Self) void {
         self.cur_t = .{ .has_newline = false,
             .kind = .invalid,
             .offset = 0,
-            .source_offset = self.cur,
+            .source_offset = @intCast(self.cur),
             .len = 0 };
-        
-        self.whitespace(); // skip trivia
+    }
 
-        // const c: u8 = self.advance();
-        
-        // switch (c) {
-            
-        // }
+    // Special handler for `:` or `:=`
+    fn handleColon(self: *Self) Token {
+        self.skip();
+        if (self.peek(0) == '=') {
+            self.skip();
+            return self.mkToken(.coloneq);
+        } else {
+            return self.mkToken(.colon);
+        }
+    }
 
+    // Special handler for `->`
+    fn handleDash(self: *Self) Token {
+        self.skip();
+        if (self.peek(0) == '>') {
+            self.skip();
+            return self.mkToken(.to);
+        } else {
+            return self.mkToken(.invalid);
+        }
+    }
+
+    // Special handler for `=>`
+    fn handleEq(self: *Self) Token {
+        self.skip();
+        if (self.peek(0) == '>') {
+            self.skip();
+            return self.mkToken(.mapsto);
+        } else {
+            return self.mkToken(.invalid);
+        }
+    }
+
+    fn identOrKeyword(self: *Self) Token {
+        // TODO: Allow hierarchical identifiers using `.`
+        // For now accept all non-ascii bytes as part of a utf-8 identifier here
+        while (isIdentCont(self.peek(0))) self.skip();
+        const token = self.src[self.cur_t.source_offset+self.cur_t.offset..self.cur];
+        if (builtinAtoms.get(token)) |kind| {
+            return self.mkToken(kind);
+        } else {
+            // TODO: validate identifier
+            return self.mkToken(.ident);
+        }
+    }
+
+    fn numLit(self: *Self) Token {
+        while (isNumLit(self.peek(0))) self.skip();
+        return self.mkToken(.numlit);
+    }
+
+    /// Finalize current token assuming that we just read its last byte.
+    fn mkToken(self: *Self, kind: TokenKind) Token {
+        self.cur_t.kind = kind;
+        self.cur_t.len = @intCast(self.cur - self.cur_t.source_offset);
+        return self.cur_t;
     }
 
     /// Skip ahead until non-whitespace detected
@@ -174,16 +271,18 @@ pub const Lexer = struct {
             while (isWhitespace(self.peek(0))) self.skip();
             const c = self.peek(0);            
             switch (c) {
-                '-' => self.singleLineComment(),
-                '/' => self.multilineComment(),
+                '-' => if (self.peek(1) == '-') self.singleLineComment() else break,
+                '/' => if (self.peek(1) == '-') self.multilineComment() else break,
                 else => break
             }
         }
+        // update token data
+        const ws_len = self.cur - self.cur_t.source_offset;
+        self.cur_t.offset = @intCast(ws_len);
     }
 
     /// Skip multiline comment. TODO: handle nested comments
     fn multilineComment(self: *Self) void {
-        if (self.peek(1) != '-') return;
         self.skip(); self.skip();
         var c = self.advance();
         while (c != '-' or self.peek(0) != '/') {
@@ -195,7 +294,6 @@ pub const Lexer = struct {
 
     /// Skip single line comment.
     fn singleLineComment(self: *Self) void {
-        if (self.peek(1) != '-') return;
         self.skip(); self.skip();
         var c = self.advance();
         while (c != '\n') {
@@ -209,16 +307,16 @@ pub const Lexer = struct {
         return self.cur == self.src.len;
     }
 
-    /// Return true if provided slice is found at current position. 
-    /// Maybe not needed
-    fn match(self: *Self, comptime c: []const u8) bool {
-        if (self.cur + c.len >= self.src.len) return false;
-        if (c.len == 1) {
-            return self.src[self.cur] == c[0];
-        } else {
-            return std.mem.eql(u8, self.src[self.cur..self.cur+c.len], c);
-        }
-    }
+    // /// Return true if provided slice is found at current position. 
+    // /// Maybe not needed
+    // fn match(self: *Self, comptime c: []const u8) bool {
+    //     if (self.cur + c.len > self.src.len) return false;
+    //     if (c.len == 1) {
+    //         return self.src[self.cur] == c[0];
+    //     } else {
+    //         return std.mem.eql(u8, self.src[self.cur..self.cur+c.len], c);
+    //     }
+    // }
 
     /// Peek a certain number of bytes ahead.
     fn peek(self: *Self, comptime offset: usize) u8 {
@@ -258,26 +356,23 @@ pub const Lexer = struct {
         return res;
     }
 
+    // fn skipFull(self: *Self) void {
+    //     _ = self.advanceFull();
+    // }
+
     fn handleutf8error(self: *Self) []const u8 {
         // Todo: return unknown character codepoint and advance to next start byte
         _ = self;
-        @panic("malformed utf-8");
+        utf8error();
     }
 
     test "Lexer.advanceFull" {
         const src = "→∀=";
         var l = Lexer.init(src);
+        l.initToken();
         try std.testing.expect(std.mem.eql(u8, l.advanceFull(), "→"));
         try std.testing.expect(std.mem.eql(u8, l.advanceFull(), "∀"));
         try std.testing.expect(std.mem.eql(u8, l.advanceFull(), "="));
-    }
-
-    fn mkEmptyEOF(self: *Self) Token {
-        return .{ .kind = .eof, 
-            .has_newline = false,
-            .offset = 0,
-            .len = 0,
-            .source_offset = self.src.len };
     }
 
 };
